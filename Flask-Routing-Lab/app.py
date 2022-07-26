83e371a850b9db853bcc9063bcc68cdca8d8b17c
watanas23-meet <pintelzyt@gmail.com>
2022-07-26 16:45:52 +0300

    commit-final-done
    holyyy potato

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -18,6 +18,11 @@ def home():
 def product():
     return render_template("product.html")
 
+@app.route("/cart")
+def cart():
+    return render_template("cart.html")
+
+
 # Your code should be above
 
 if __name__ == "__main__":  # Makes sure this is the main process
